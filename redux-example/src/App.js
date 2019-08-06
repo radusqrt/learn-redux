eea7082e749f2254213af74c86034736ede344ee
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2019-08-06 08:41:38 +0300

    Redux app

---
+++ redux-example/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+// The glue for Redux
+import { Provider } from "react-redux";
+
+import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
+
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
